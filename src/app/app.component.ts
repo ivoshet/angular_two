37f07a78ac5d515ce0f0d80e9ca03c8b1ae74004
ivoshet <ivan.v.shet@gmail.com>
2020-11-22 22:17:50 +0500

    added hand make component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <h1>PonyRacer {{user?.name}}</h1>
+  <ns-racer></ns-racer>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-first-app';
+  user: any = {name: 'Cedric'};
 }
